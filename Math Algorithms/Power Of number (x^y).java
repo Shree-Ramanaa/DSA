f2769ba3ca32f1412e8bd0eb45936f6d6958f715
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2022-09-10 15:09:27 +0530

    Create Power Of number (x^y).java

---
+++ Math Algorithms/Power Of number (x^y).java
@@ -0,0 +1,11 @@
+// O(log(Y))
+long binpow(long a, long b) {
+    long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
