79b701a0735eb9626fa9158e00792c4e194aa4d8
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2022-09-11 22:21:49 +0530

    Update nCr - Pascal's Triangle.java

--- Math Algorithms/nCr - Pascal's Triangle.java
+++ Math Algorithms/nCr - Pascal's Triangle.java
@@ -1,16 +1,30 @@
-public ArrayList<ArrayList<Integer>> getCombinationTable(int N){
-    ArrayList<ArrayList<Integer>> pascalTriangle= new ArrayList();
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt() , r = sc.nextInt();
+	    ArrayList<ArrayList<Integer>> nCr =  getCombinationTable(x);
+	    System.out.println(nCr.get(n).get(r));
+        
+	}
+	
+	public static ArrayList<ArrayList<Integer>> getCombinationTable(int N){
+    
+        ArrayList<ArrayList<Integer>> pascalTriangle= new ArrayList();
 
-    for(int i=0; i<=N; i++){
-        ArrayList<Integer> pascalRow = new ArrayList();
-        for(int j=0; j<=i; j++){
-            if(j==0 || j==i){
-                pascalRow.add(1);
-            }else{
-                pascalRow.add(pascalTriangle.get(i-1).get(j) + pascalTriangle.get(i-1).get(j-1));
+        for(int i=0; i<=N; i++){
+            ArrayList<Integer> pascalRow = new ArrayList();
+            for(int j=0; j<=i; j++){
+                if(j==0 || j==i){
+                    pascalRow.add(1);
+                }else{
+                    pascalRow.add(pascalTriangle.get(i-1).get(j) + pascalTriangle.get(i-1).get(j-1));
+                }
             }
+            pascalTriangle.add(pascalRow);
         }
-        pascalTriangle.add(pascalRow);
+        return pascalTriangle;
     }
-    return pascalTriangle;
 }
