938cb288ad15d0dabadd5bc6ef0dfb16d9c5a1c8
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2022-09-20 13:10:02 +0530

    Create bucket sort.java

---
+++ Sorting Algorithms/bucket sort.java
@@ -0,0 +1,21 @@
+void bucket(int a[], int n) // function to implement bucket sort  
+{  
+  int max = getMax(a, n); //max is the maximum element of array  
+  int bucket[max], i;  
+  for (int i = 0; i <= max; i++)  
+  {  
+    bucket[i] = 0;  
+  }  
+  for (int i = 0; i < n; i++)  
+  {  
+    bucket[a[i]]++;  
+  }  
+  for (int i = 0, j = 0; i <= max; i++)  
+  {  
+    while (bucket[i] > 0)  
+    {  
+      a[j++] = i;  
+      bucket[i]--;  
+    }  
+  }  
+}  
