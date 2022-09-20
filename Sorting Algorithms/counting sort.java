85672d40f715206273c310d4ba204732f74c6840
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2022-09-20 13:07:34 +0530

    Create counting sort.java

---
+++ Sorting Algorithms/counting sort.java
@@ -0,0 +1,29 @@
+void countingSort(int a[], int n) 
+{  
+   int output[n+1];  
+   int max = getMax(a, n);  
+   int count[max+1]; 
+  
+  
+  for (int i = 0; i <= max; ++i)   
+  {  
+    count[i] = 0;
+  }  
+    
+  for (int i = 0; i < n; i++)
+  {  
+    count[a[i]]++;  
+  }  
+  
+   for(int i = 1; i<=max; i++)   
+      count[i] += count[i-1];
+ 
+  for (int i = n - 1; i >= 0; i--) {  
+    output[count[a[i]] - 1] = a[i];  
+    count[a[i]]--; 
+  }  
+  
+   for(int i = 0; i<n; i++) {  
+      a[i] = output[i]; 
+   }  
+}  
