558727493eeeb6e2384443e2d6ffe7de7788c74b
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2022-10-21 19:26:06 +0530

    Create 206.reverse a linkedList.java

---
+++ LinkedList/206.reverse a linkedList.java
@@ -0,0 +1,7 @@
+private ListNode reverseListInt(ListNode head, ListNode newHead) {
+    if (head == null)
+        return newHead;
+    ListNode next = head.next;
+    head.next = newHead;
+    return reverseListInt(next, head);
+}
